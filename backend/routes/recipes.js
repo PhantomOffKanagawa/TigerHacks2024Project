c60c44ce632135f49410f473163c38fbd636d578
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2024-11-02 16:41:58 -0500

    Add recipes/user API

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,25 +1,26 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import { db } from '../config/firebase.js';
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
+dotenv.config();
 const router = express.Router();
 
-router.get('/:url', async function (req, res) {
-    const url = 'https://scgcplb7osrk65nbxgnzp43cz40jekxj.lambda-url.us-east-2.on.aws/';
+router.post('/', async function (req, res) {
+    const { url, userId } = req.body;
 
-    const requestedUrl = decodeURIComponent(req.params.url);
-
-    const requestBody = {
-        url: requestedUrl
-    };
+    if (!url || !userId) {
+        return res.status(400).send({ error: 'URL and userId are required' });
+    }
 
     try {
-        const response = await fetch(url, {
+        const response = await fetch(process.env.RECIPE_SCRAPER_SERVICE_URL, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(requestBody),
+            body: JSON.stringify({ url }),
         });
 
         if (!response.ok) {
@@ -29,21 +30,53 @@ router.get('/:url', async function (req, res) {
         const data = await response.json();
         console.log(data);
 
-        const recipeQuery = await db.collection('recipes').where('canonical_url', '==', data.canonical_url).get();
+        let recipeRef;
+        const recipeQuery = await db.collection('recipes')
+            .where('canonical_url', '==', data.canonical_url)
+            .limit(1)
+            .get();
 
         if (!recipeQuery.empty) {
-            const existingRecipe = recipeQuery.docs[0].data();
-            return res.send(existingRecipe);
+            recipeRef = recipeQuery.docs[0].ref;
+        } else {
+            recipeRef = db.collection('recipes').doc();
+            await recipeRef.set(data);
         }
 
-        const recipeRef = db.collection('recipes').doc();
-        await recipeRef.set(data);
+        const userRef = db.collection('users').doc(userId);
+        await userRef.update({
+            recipeIDs: admin.firestore.FieldValue.arrayUnion(recipeRef.id),
+        })
 
-        res.send(data);
+        res.status(201).send({ message: 'Recipe saved successfully', recipeId: recipeRef.id });
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
         res.status(500).send({ error: 'Internal Server Error' });
     }
 });
+
+router.get('/user/:userID', async (req, res) => {
+    const { userID } = req.params;
+
+    try {
+        const userDoc = await db.collection('users').doc(userID).get();
+
+        if (!userDoc.exists) {
+            return res.status(404).send({ error: 'User not found' });
+        }
+
+        const { recipeIDs } = userDoc.data();
+
+        const recipesQuery = await db.collection('recipes')
+            .where(admin.firestore.FieldPath.documentId(), 'in', recipeIDs)
+            .get();
+
+        const recipes = recipesQuery.docs.map(doc => ({id: doc.id, ...doc.data()}));
+        res.send(recipes);
+    } catch (error) {
+        console.error('Error fetching recipes for user:', error);
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+})
 
 export default router;
