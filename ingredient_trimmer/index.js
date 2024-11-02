0a1696c044e49bb59c88e5c6327f84a7be37a3a5
Zack Murry <zackk.murryy@gmail.com>
2024-11-01 23:55:09 -0500

    Add fuzzy search to ingredient trimmer

--- ingredient_trimmer/index.js
+++ ingredient_trimmer/index.js
@@ -1,18 +1,24 @@
 import fs from 'fs'
+import Fuse from 'fuse.js'
 
-const data = JSON.parse(fs.readFileSync('./response.json', 'utf8'));
+const scrapedData = JSON.parse(fs.readFileSync('./response.json', 'utf8'));
+//const scrapedData = JSON.parse(fs.readFileSync('./chicken_response.json', 'utf8'));
+//const scrapedData = JSON.parse(fs.readFileSync('./pasta_response.json', 'utf8'));
 
-const { ingredients } = data
+const { ingredients } = scrapedData
 
 console.log(ingredients)
 
 function simplifyIngredient(ingredient) {
-    // Remove any content inside parentheses
-    let cleaned = ingredient.replace(/\(.*?\)/g, '');
+    //let cleaned = ingredient.replace(/\(.*?\)/g, '');
+    // Remove content inside parentheses or any text following an unmatched opening parenthesis
+    let cleaned = ingredient.replace(/\(.*?\)|\(.*$/g, '');
 
     // Remove measurements and quantities (e.g., "1 1/2 cups", "6.75 ounces", etc.)
-    cleaned = cleaned.replace(/\b\d+([\/.\d]*)?\s*(cups?|tablespoons?|teaspoons?|ounces?|grams?|milliliters?|liters?|pinches?|dash(es)?|pounds?|lbs?|kgs?|grams?|g|ml|oz)\b/gi, '');
-    cleaned = cleaned.replace(/\bof\b/g, '').replace(/\d+/g, '').trim();
+    cleaned = cleaned.replace(/\b\d+([\/.\d]*)?\s*(cups?|tbsp|tsp|tablespoon(s)?|teaspoon(s)?|ounce(s)?|gram(s)?|milliliters?|liter(s)?|pinch(es)?|dash(es)?|jar(s)?|pound(s)?|lb(s)?|kg(s)?|gram(s)?|g|ml|oz)\b/gi, '');
+    cleaned = cleaned.replace(/\b(fresh|sliced)\b/gi, '').replace(/\s{2,}/g, ' ')
+    cleaned = cleaned.replace(/[\d¼½¾⅓⅔⅛⅜⅝⅞]/gi, '').replace(/\bteaspoon\b/gi, ' ')
+    cleaned = cleaned.replace(/\bof\b\s*/g, '').replace(/\d+/g, '').trim();
 
     // Remove any remaining numbers and extra spaces
     cleaned = cleaned.replace(/\d+/g, '').trim();
@@ -23,7 +29,28 @@ function simplifyIngredient(ingredient) {
     return cleaned
 }
 
-console.log(ingredients.map(simplifyIngredient))
+const trimmed = ingredients.map(simplifyIngredient)
+console.log(trimmed)
+
+
+const allFoods = JSON.parse(fs.readFileSync('./gh_ings.json', 'utf8'))['foods'];
+//const allFoods = JSON.parse(fs.readFileSync('./ingredient_list.json', 'utf8'))['foods'];
+// console.log(allFoods)
+
+const fuse = new Fuse(allFoods, {
+  includeScore: true
+})
+
+// const result = fuse.search('packed light brown sugar')
+for (let ing of trimmed) {
+  const result = fuse.search(ing)
+  if (!result.length) {
+    console.log(ing + " has no results")
+    continue;
+  }
+  console.log(result[0])
+}
+// console.log(result)
 
 export {}
 
