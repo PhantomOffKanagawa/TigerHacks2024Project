9e913d877fb31b524aec97fba9c37b0033ba28ee
Zack Murry <zackk.murryy@gmail.com>
2024-11-03 00:37:54 -0500

    Fix carbon score null handling

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -96,7 +96,7 @@ router.post('/save', async function (req, res) {
       data['carbonData'] = carbonData
       data['averageCarbonScore'] = avg
       data['sanitizedIngredients'] = sanitizeIngredients(data.ingredients)
-      if (Object.keys(carbonData).length === 0 || averageCarbonScore === 0) {
+      if (Object.keys(carbonData).length === 0 || avg === 0) {
         return res.status(400).send({ error: 'Failed to get carbon data'})
       }
       await recipeRef.set(data)
@@ -218,23 +218,43 @@ router.get('/substitutions/:userId/:recipeId', async function (req, res) {
 });
 
 router.get('/all', async function (req, res) {
-  const { userID } = req.params
-
   try {
-    let recipesQuery = null
-    try {
-      recipesQuery = await db
-        .collection('recipes')
-        .get()
-    } catch (e) {
-      return res.status(500).send([])
+    // Parse page number from query parameters (default to page 1)
+    const page = parseInt(req.query.p) || 1;
+
+    // Set pagination parameters
+    const pageSize = 3;
+    let lastVisible = null;
+
+    // Reference the recipes collection
+    const recipesRef = db.collection('recipes');
+
+    // Query to order by ID and limit to pageSize
+    let recipesQuery = recipesRef.orderBy('title').limit(pageSize);
+
+    if (page > 1) {
+      const skipQuery = recipesRef.orderBy('title').limit((page - 1) * pageSize);
+      const skipSnapshot = await skipQuery.get();
+
+      // Check if we have enough documents for pagination
+      if (skipSnapshot.docs.length < (page - 1) * pageSize) {
+        return res.status(404).json({ error: 'Page not found' });
+      }
+
+      // Set `lastVisible` to the last document from the skip query
+      const lastVisible = skipSnapshot.docs[skipSnapshot.docs.length - 1];
+      recipesQuery = recipesRef.orderBy('title').startAfter(lastVisible).limit(pageSize);
     }
 
-    const recipes = recipesQuery.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }))
-    res.send(recipes)
+
+    // Get documents from the updated query
+    const recipeSnapshot = await recipesQuery.get();
+    const recipes = recipeSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+    res.status(200).json({
+      page,
+      recipes
+    });
   } catch (error) {
     console.error('Error fetching all recipes:', error)
     res.status(500).send({ error: 'Internal Server Error' })
