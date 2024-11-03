4ae6a529999b8a3ca3a4b281c363a2ae5f77521b
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2024-11-02 23:36:06 -0500

    Add register api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,29 @@
 import express from 'express';
+import {db} from "../config/firebase.js";
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.post('/register', async function(req, res, next) {
+  const { userId } = req.body;
+
+  if (!userId) {
+    return res.status(400).send({ error: 'userId required' })
+  }
+
+  try {
+    const userRef = db.collection('users').doc(`${userId}`);
+
+    await userRef.set({
+      recipeIDs: []
+    }, { merge: true });
+
+    res.status(201).send({
+      message: 'User registered successfully'
+    })
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error)
+    res.status(500).send({ error: 'Internal Server Error' })
+  }
 });
 
 export default router;
